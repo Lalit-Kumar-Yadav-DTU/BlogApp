9759a89a31991e94d2f5ea31e9c49b39c3e6f495
Lalit Kumar Yadav <lalitkumaryadav14112003@gmail.com>
2026-01-15 20:01:01 +0530

    Changing error messages for better user experience

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,15 +4,15 @@ exports.createPost = async (req, res) => {
     try{
         const {title, body} = req.body;
         const post = new Post({title, body });
-        const savedPost = await post.save();
+        const postSaved = await post.save();
 
         res.json({
-            post : savedPost
+            post : postSaved
         })
     }
     catch(err){
         return res.status(400).json({
-            error : "Error While Creating Post"
+            error : "Error When Making This Post"
         })
     }
 }
